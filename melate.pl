33edbfce688bb5961461565f5791c4b971672f87
Fernando Romo <pop@cofradia.org>
2024-04-30 21:56:43 -0600

    GPL comment

--- melate.pl
+++ melate.pl
@@ -1,17 +1,30 @@
 #!/usr/bin/perl
-#=====================================================================#
-# Program => melate.pl (In Perl 5.0)                    version 1.0.0 #
-#=====================================================================#
-# Autor         => Fernando "El Pop" Romo          (pop@cofradia.org) #
-# Creation date => 22/April/2024                                      #
-#---------------------------------------------------------------------#
-# Info => This program gives statistical information of the numbers   #
-#         of the mexican version of lotto.                            #
-#---------------------------------------------------------------------#
-# This code are released under the GPL 3.0 License. Any change must   #
-# be report to the authors                                            #
-#                     (c) 2024 - Fernando Romo                        #
-#=====================================================================#
+#======================================================================#
+# Program => melate.pl (In Perl 5.0)                     version 1.0.0 #
+#======================================================================#
+# Autor         => Fernando "El Pop" Romo           (pop@cofradia.org) #
+# Creation date => 22/April/2024                                       #
+#----------------------------------------------------------------------#
+# Info => This program gives statistical information of the numbers    #
+#         of the mexican version of lotto.                             #
+#----------------------------------------------------------------------#
+#        This code are released under the GPL 3.0 License.             #
+#                                                                      #
+#                     (c) 2024 - Fernando Romo                         #
+#                                                                      #
+# This program is free software: you can redistribute it and/or modify #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# This program is distributed in the hope that it will be useful, but  #  
+# WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    #
+# General Public License for more details.                             #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with this program. If not, see <https://www.gnu.org/licenses/> #
+#======================================================================#
 use strict;
 use DBI;            # Interface to Database
 use File::Copy;     # File cp or mv
