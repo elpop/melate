96352559a61c5641881a1951d693a64e8858b379
Fernando Romo <pop@cofradia.org>
2024-10-24 10:54:18 -0600

    sample of manual entry

---
+++ manual_entry_sample.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+melate.pl -add product=melate     draw=3903 date='2024-05-22' balls='6,20,25,26,32,35,51' award=248700000
+melate.pl -add product=revancha   draw=3903 date='2024-05-22' balls='14,24,27,31,37,41'   award=24500000
+melate.pl -add product=revanchita draw=3903 date='2024-05-22' balls='6,21,23,42,43,52'    award=357100000
+
